b9c7b1956d0086997f30f9b4da59614c49e15dd5
baopham639567@gmail.com <baopham639567@gmail.com>
2021-11-16 10:30:11 +0700

    lan5

--- TraSuaWeb/Controllers/DonHangController.cs
+++ TraSuaWeb/Controllers/DonHangController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using TraSuaWeb.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using TraSuaWeb.ModelView;
 
 namespace TraSuaWeb.Controllers
 {
@@ -19,45 +20,48 @@ namespace TraSuaWeb.Controllers
             _context = context;
             _notifyService = notifyService;
         }
-        //[HttpPost]
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    try
-        //    {
-        //        var taikhoanID = HttpContext.Session.GetString("MaKh");
-        //        if (string.IsNullOrEmpty(taikhoanID))
-        //            return RedirectToAction("Login", "Accounts");
-        //        var khachhang = _context.KhachHangs.AsNoTracking().SingleOrDefault(x => x.MaKh == Convert.ToInt32(taikhoanID));
-        //        if (khachhang == null) return NotFound();
-        //        var donhang = await _context.HoaDons
-        //            .Include(x => x.TrangThai)
-        //            .FirstOrDefaultAsync(m => m.MaHd == id && Convert.ToInt32(taikhoanID) == m.MaKh);
-        //        if (donhang == null)
-        //        {
-        //            return NotFound();
-        //        }
-        //        var chitietdonhang = _context.ChiTietHoaDons
-        //            .Include(x => x.MaSp)
-        //            .AsNoTracking()
-        //            .Where(x => x.MaHd == id)
-        //            .OrderBy(x =>x.MaSize)
-        //            .ToList();
-        //        XemDonHang donHang = new XemDonHang();
-        //    }
-        //    catch
-        //    {
-        //        return NotFound();
-        //    }
-            
-        // }
+        [HttpPost]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                var taikhoanID = HttpContext.Session.GetString("MaKh");
+                if (string.IsNullOrEmpty(taikhoanID))
+                    return RedirectToAction("Login", "Accounts");
+                var khachhang = _context.KhachHangs.AsNoTracking().SingleOrDefault(x => x.MaKh == Convert.ToInt32(taikhoanID));
+                if (khachhang == null) return NotFound();
+                var donhang = await _context.HoaDons
+                    .Include(x => x.TrangThai)
+                    .FirstOrDefaultAsync(m => m.MaHd == id && Convert.ToInt32(taikhoanID) == m.MaKh);
+                if (donhang == null)
+                {
+                    return NotFound();
+                }
+                var chitietdonhang = _context.ChiTietHoaDons
+                    .Include(x => x.MaSp)
+                    .AsNoTracking()
+                    .Where(x => x.MaHd == id)
+                    .OrderBy(x => x.MaSize)
+                    .ToList();
+                XemDonHang donHang = new XemDonHang();
+                donHang.DonHang = donhang;
+                donHang.ChiTietDonHang = chitietdonhang;
+                return PartialView("Details", donHang);
+            }
+            catch
+            {
+                return NotFound();
+            }
 
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
